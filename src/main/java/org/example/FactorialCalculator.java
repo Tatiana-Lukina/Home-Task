27c447b0b4083c01790f1dca175d8e17013dc453
Tanya <tanyalykina57@gmail.com>
2024-12-23 20:52:21 +0500

    add

---
+++ src/main/java/org/example/FactorialCalculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+// Напишите программу, позволяющую вычислить факториал числа. Напишите юнит-тесты для этой программы, используя Junit 5.
+public class FactorialCalculator {
+    public long calculateFactorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Число должно быть неотрицательным");
+        }
+        long result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
