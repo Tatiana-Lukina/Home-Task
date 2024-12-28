ecaa61b1c5ef000fd23f014c0649119bf46b74a9
Tanya <tanyalykina57@gmail.com>
2024-12-28 12:47:34 +0500

    first

---
+++ src/test/java/org/example/BaserTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaserTest {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeEach
+    public void setupTest() {
+        // Создаём экземпляр драйвера
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        // Растягиваем окно браузера
+        driver.manage().window().maximize();
+        // Переход на страницу
+        driver.get("https://www.mts.by/");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
